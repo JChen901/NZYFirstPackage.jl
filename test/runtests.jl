cdfc4e893c81a926ee2853b3da61858dbbb67b9d
nzy <nzy@nzy-3.local>
2024-02-29 19:12:14 +0800

    save

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using MyFirstPackage
 using Test
 
-@testset "MyFirstPackage.jl" begin
-    # Write your tests here.
+@testset "flip_direction_index" begin
+    d=D2Q9()
+    @test flip_direction_index(d, 1) == 9
 end
